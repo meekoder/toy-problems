abb2255b630de1d762c796f85836fbe4cc1ba526
meekoder <meekorusdi@gmail.com>
2021-06-02 12:38:26 -0700

    sortSentence

---
+++ leetcode/easy/sortSentence.js
@@ -0,0 +1,14 @@
+/**
+ * @param {string} s
+ * @return {string}
+ */
+var sortSentence = function(s) {
+  const split = s.split(' ');
+  const results = new Array(split.length);
+  for (let i = 0; i < split.length; i++) {
+    const curr = split[i];
+    const idx = curr.slice(curr.length - 1) - 1;
+    results[Number(idx)] = curr.slice(0, curr.length - 1);
+  }
+  return results.join(' ');
+};
